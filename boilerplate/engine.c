a77f63ac96c449cddb64a27a7f54d90b8ca0c0d5
Arpita Pillai <pillai.arpitab@gmail.com>
2026-04-15 08:13:51 +0000

    Implement bounded buffer push and pop functions

--- boilerplate/engine.c
+++ boilerplate/engine.c
@@ -289,9 +289,23 @@ static void bounded_buffer_begin_shutdown(bounded_buffer_t *buffer)
  */
 int bounded_buffer_push(bounded_buffer_t *buffer, const log_item_t *item)
 {
-    (void)buffer;
-    (void)item;
-    return -1;
+    pthread_mutex_lock(&buffer->mutex);
+
+    while (buffer->count == LOG_BUFFER_CAPACITY && !buffer->shutting_down)
+        pthread_cond_wait(&buffer->not_full, &buffer->mutex);
+
+    if (buffer->shutting_down) {
+        pthread_mutex_unlock(&buffer->mutex);
+        return -1;
+    }
+
+    buffer->items[buffer->tail] = *item;
+    buffer->tail = (buffer->tail + 1) % LOG_BUFFER_CAPACITY;
+    buffer->count++;
+
+    pthread_cond_signal(&buffer->not_empty);
+    pthread_mutex_unlock(&buffer->mutex);
+    return 0;
 }
 
 /*
@@ -305,9 +319,23 @@ int bounded_buffer_push(bounded_buffer_t *buffer, const log_item_t *item)
  */
 int bounded_buffer_pop(bounded_buffer_t *buffer, log_item_t *item)
 {
-    (void)buffer;
-    (void)item;
-    return -1;
+    pthread_mutex_lock(&buffer->mutex);
+
+    while (buffer->count == 0) {
+        if (buffer->shutting_down) {
+            pthread_mutex_unlock(&buffer->mutex);
+            return -1;
+        }
+        pthread_cond_wait(&buffer->not_empty, &buffer->mutex);
+    }
+
+    *item = buffer->items[buffer->head];
+    buffer->head = (buffer->head + 1) % LOG_BUFFER_CAPACITY;
+    buffer->count--;
+
+    pthread_cond_signal(&buffer->not_full);
+    pthread_mutex_unlock(&buffer->mutex);
+    return 0;
 }
 
 /*
@@ -321,7 +349,34 @@ int bounded_buffer_pop(bounded_buffer_t *buffer, log_item_t *item)
  */
 void *logging_thread(void *arg)
 {
-    (void)arg;
+    supervisor_ctx_t *ctx = (supervisor_ctx_t *)arg;
+    log_item_t item;
+
+    while (bounded_buffer_pop(&ctx->log_buffer, &item) == 0) {
+        char log_path[PATH_MAX];
+        log_path[0] = '\0';
+
+        pthread_mutex_lock(&ctx->metadata_lock);
+        container_record_t *rec = ctx->containers;
+        while (rec) {
+            if (strncmp(rec->id, item.container_id, CONTAINER_ID_LEN) == 0) {
+                strncpy(log_path, rec->log_path, PATH_MAX - 1);
+                break;
+            }
+            rec = rec->next;
+        }
+        pthread_mutex_unlock(&ctx->metadata_lock);
+
+        if (log_path[0] == '\0')
+            continue;
+
+        int fd = open(log_path, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd < 0)
+            continue;
+        write(fd, item.data, item.length);
+        close(fd);
+    }
+
     return NULL;
 }
 
